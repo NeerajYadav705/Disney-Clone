75c82311e3e3fce61c5755a068429fdf3c4b6749
NeerajYadav <neeraj95578@gmail.com>
2024-02-05 21:50:36 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import Header from "@/components/Header";
+import { ThemeProvider } from "@/components/ThemeProvider";
+
+
+
+export const metadata: Metadata = {
+  title: "Disney Clone",
+  description: "Naukri ke liye project",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="bg-white dark:bg-[#1a1c29]">
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+        <Header/>
+        {children}
+        </ThemeProvider>
+        </body>
+    </html>
+  );
+}
